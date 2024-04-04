989de9f1586df8df415b809fe105869d695f4f19
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-04-04 21:05:42 +0300

    search styles

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,13 @@
 import { useContext } from 'react';
 import { SearchContext } from '../../App';
+import { LiaSearchSolid } from 'react-icons/lia';
 
 export const Search = () => {
   const { searchValue, setSearchValue } = useContext(SearchContext);
 
   return (
     <div className="search-input">
+      <LiaSearchSolid className="search-icon" />
       <input
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
