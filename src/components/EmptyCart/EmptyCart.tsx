3010127d67b1a041b1b84a4a2bc04b9770777772
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-10-19 18:54:47 +0300

    update formatting in EmptyCart folder

---
+++ src/components/EmptyCart/EmptyCart.tsx
@@ -0,0 +1,24 @@
+import { IoCartOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+
+import styles from './EmptyCart.module.scss';
+
+const EmptyCart = () => {
+  return (
+    <>
+      <div>
+        <h2 className={styles.title}>Cart</h2>
+      </div>
+      <div className={styles.empty}>
+        <IoCartOutline className={styles.icon} size={60} />
+        <h2>Cart is empty</h2>
+        <p>Return to the main page to order items:</p>
+        <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+          <button>Home</button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default EmptyCart;
